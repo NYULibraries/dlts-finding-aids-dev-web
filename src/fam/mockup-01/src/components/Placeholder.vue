e9ceb94cb7f9ebcaeb9293ca139a37d3e6983093
David <da70@nyu.edu>
2020-02-24 17:03:44 -0500

    src/: replace with placeholder application

---
+++ src/fam/mockup-01/src/components/Placeholder.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>Finding Aids Manager - Interactive Mockup #1</h1>
+        Coming soon
+    </div>
+</template>
+
+<script>
+export default {
+    name  : 'Placeholder',
+    props : {
+        msg : {
+            type     : String,
+            default  : null,
+        },
+    },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/fam/mockup-01/src/views/Home.vue
+++ src/fam/mockup-01/src/views/Home.vue
@@ -1,21 +1,17 @@
 <template>
-    <div class="home">
-        <img
-            alt="Vue logo"
-            src="../assets/logo.png"
-        >
-        <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+        <Placeholder />
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Placeholder from '@/components/Placeholder.vue';
 
 export default {
     name       : 'Home',
     components : {
-        HelloWorld,
+        Placeholder,
     },
 };
 </script>
