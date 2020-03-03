f550f31cc878349d64cff784c028d84515d8cb5c
David <da70@nyu.edu>
2020-03-03 14:25:08 -0500

    fam/mockup-01/main.js: add BootstrapVue and BootstrapVueIcons

--- src/fam/mockup-01/src/main.js
+++ src/fam/mockup-01/src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
 import App from './App.vue';
 import router from './router';
 import storeConfig from './store/store-config';
 
 Vue.config.productionTip = false;
 
+Vue.use( BootstrapVue );
+Vue.use( BootstrapVueIcons );
 Vue.use( Vuex );
 const store = new Vuex.Store( storeConfig );
 
