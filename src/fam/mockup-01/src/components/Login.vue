f9e2f3b99cbf514aa4a34769a537bf235970a7bd
David <da70@nyu.edu>
2020-03-30 19:19:25 -0400

    FAM mockup 1: add Login shell

---
+++ src/fam/mockup-01/src/components/Login.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h1>Finding Aids Manager - Interactive Mockup #1</h1>
+
+        <h2>Login component</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name  : 'Login',
+    props : {
+        msg : {
+            type     : String,
+            default  : null,
+        },
+    },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
