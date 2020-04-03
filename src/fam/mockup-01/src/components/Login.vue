00004e2ffb36d2a6cba6dd8951435384ae3207a3
David <da70@nyu.edu>
2020-04-03 18:51:30 -0400

    FAM mockup 1: basic login with alert message

--- src/fam/mockup-01/src/components/Login.vue
+++ src/fam/mockup-01/src/components/Login.vue
@@ -6,39 +6,52 @@
         >
             <h1>Welcome to the Finding Aids Manager (FAM)</h1>
 
-            <h2 id="login-header">
-                Login
-            </h2>
+            <b-form @submit.prevent="clickSignIn">
+                <b-row
+                    id="main-row"
+                    align-h="center"
+                >
+                    <b-col cols="2">
+                        <label for="username">Username</label>
+                    </b-col>
+                    <b-col cols="4">
+                        <b-form-input
+                            id="username"
+                            v-model="username"
+                            type="email"
+                        />
+                    </b-col>
+                </b-row>
 
-            <b-row
-                id="main-row"
-                align-h="center"
-            >
-                <b-col cols="2">
-                    <label for="username">Username</label>
-                </b-col>
-                <b-col cols="4">
-                    <b-form-input
-                        id="username"
-                        type="email"
-                    />
-                </b-col>
-            </b-row>
+                <b-row
+                    align-h="center"
+                    class="my-1"
+                >
+                    <b-col cols="2">
+                        <label for="password">Password</label>
+                    </b-col>
+                    <b-col cols="4">
+                        <b-form-input
+                            id="password"
+                            v-model="password"
+                            type="password"
+                        />
+                    </b-col>
+                </b-row>
 
-            <b-row
-                align-h="center"
-                class="my-1"
-            >
-                <b-col cols="2">
-                    <label for="password">Password</label>
-                </b-col>
-                <b-col cols="4">
-                    <b-form-input
-                        id="password"
-                        type="password"
-                    />
-                </b-col>
-            </b-row>
+                <b-row
+                    align-h="center"
+                    class="my-1"
+                >
+                    <b-col cols="6">
+                        <button
+                            id="sign-in-button"
+                        >
+                            Sign In
+                        </button>
+                    </b-col>
+                </b-row>
+            </b-form>
         </b-container>
     </div>
 </template>
@@ -48,12 +61,27 @@ import { mapGetters } from 'vuex';
 
 export default {
     name     : 'Login',
+    data() {
+        return {
+            username : '',
+            password : '',
+        };
+    },
     computed : {
         ...mapGetters(
             [
                 'users',
             ],
         ),
+    },
+    methods : {
+        clickSignIn() {
+            if ( this.users.includes( this.username ) && this.password === 'password' ) {
+                alert( 'User ' + this.username + ' has signed in.' );
+            } else {
+                alert( 'Invalid user and password combination' );
+            }
+        },
     },
 };
 </script>
@@ -69,6 +97,10 @@ export default {
 }
 
 #main-row {
+    margin-top: 5%;
+}
+
+#sign-in-button {
     margin-top: 5%;
 }
 </style>
