9f66f345d61f57c94b47d5270bdacac517d1384d
David <da70@nyu.edu>
2020-04-07 13:43:33 -0400

    FAM mockup 1: add repositories to store

--- src/fam/mockup-01/src/store/store-config.js
+++ src/fam/mockup-01/src/store/store-config.js
@@ -8,6 +8,48 @@ const state = function () {
             content : '',
             title   : '',
         },
+        repositories : {
+            'akkasah' : [
+                'weatherly@nyu.edu',
+                'brad@nyu.edu',
+            ],
+            'archives' : [
+                'weatherly@nyu.edu',
+                'megan@nyu.edu',
+            ],
+            'bhs' : [
+                'weatherly@nyu.edu',
+                'maggie@brooklynhistory.org',
+            ],
+            'fales' : [
+                'weatherly@nyu.edu',
+                'megan@nyu.edu',
+            ],
+            'lapietra' : [
+                'weatherly@nyu.edu',
+                'francesca@nyu.edu',
+            ],
+            'nyhs' : [
+                'weatherly@nyu.edu',
+                'larry@nyhistory.org',
+            ],
+            'nyuad' : [
+                'weatherly@nyu.edu',
+                'brad@nyu.edu',
+            ],
+            'poly' : [
+                'weatherly@nyu.edu',
+                'lyndsay@nyu.edu',
+            ],
+            'rism' : [
+                'weatherly@nyu.edu',
+                'megan@nyu.edu',
+            ],
+            'tamwag' : [
+                'weatherly@nyu.edu',
+                'megan@nyu.edu',
+            ],
+        },
         // https://jira.nyu.edu/jira/browse/FADESIGN-63
         users : [
             'brad@nyu.edu',
