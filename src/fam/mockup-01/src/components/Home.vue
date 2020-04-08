d120a7e12315373e5b0025c56e545ba1689acd61
David <da70@nyu.edu>
2020-04-08 17:58:54 -0400

    FAM mockup 1: create basic Home UI

--- src/fam/mockup-01/src/components/Home.vue
+++ src/fam/mockup-01/src/components/Home.vue
@@ -1,37 +1,85 @@
 <template>
-    <div>
-        <h1>Finding Aids Manager - Interactive Mockup #1</h1>
-
-        <h2>Home component</h2>
-    </div>
+    <b-container
+        id="main-container"
+        class="main-container h-100"
+    >
+        <b-row
+            id="welcome-row"
+            align-v="center"
+        >
+            <b-col>
+                <h1>
+                    Welcome to the FAM
+                </h1>
+            </b-col>
+        </b-row>
+        <b-row
+            id="options-row"
+        >
+            <b-col
+                class="option-column"
+                cols="4"
+            >
+                <router-link
+                    class="option-link"
+                    :to="{ name : 'create' }"
+                >
+                    Create a finding aid
+                </router-link>
+            </b-col>
+            <b-col
+                class="option-column"
+                cols="4"
+            >
+                <router-link
+                    class="option-link"
+                    :to="{ name : 'in-process' }"
+                >
+                    Manage in-process finding aids
+                </router-link>
+            </b-col>
+            <b-col
+                class="option-column"
+                cols="4"
+            >
+                <router-link
+                    class="option-link"
+                    :to="{ name : 'published' }"
+                >
+                    Manage published finding aids
+                </router-link>
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
 export default {
     name  : 'Home',
     props : {
-        msg : {
-            type     : String,
-            default  : null,
-        },
     },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.option-column {
+    border : solid black 1px;
+    /* Height of row = 900px * 18% */
+    line-height : 162px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.option-link {
+    font-size : 24px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+#options-row {
+    height : 18%;
 }
-a {
-  color: #42b983;
+
+#welcome-row {
+    border : solid black 1px;
+    height     : 18%;
+    margin-top : 20%;
 }
 </style>
