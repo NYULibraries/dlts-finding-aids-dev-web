ba47ae89a61ad3d789781935c23a3561f7928870
David <da70@nyu.edu>
2020-04-09 14:30:39 -0400

    FAM mockup 1: start basic UI for CreateNewFindingAid

--- src/fam/mockup-01/src/components/CreateNewFindingAid.vue
+++ src/fam/mockup-01/src/components/CreateNewFindingAid.vue
@@ -7,13 +7,71 @@
             <navbar />
 
             <h1 id="header">
-                Create or update a finding aid
+                Upload EAD XML file
             </h1>
+
+            <b-row
+                id="upload-row"
+                class="row"
+                align-h="center"
+            >
+                <b-col
+                    id="upload-col"
+                >
+                    Upload
+                </b-col>
+            </b-row>
+
+            <b-row
+                id="results-row"
+                class="row"
+                align-h="center"
+            >
+                <b-col
+                    id="results-col"
+                >
+                    <b-form-textarea
+                        id="textarea"
+                        v-model="results"
+                        placeholder="Waiting for upload of EAD file..."
+                        rows="20"
+                        readonly
+                    />
+                </b-col>
+            </b-row>
+
+            <b-row
+                id="buttons-row"
+                class="row"
+                align-h="center"
+            >
+                <b-col
+                    id="buttons-col"
+                    cols="10"
+                >
+                    <b-button
+                        id="cancel-button"
+                        class="button"
+                        variant="dark"
+                    >
+                        Cancel
+                    </b-button>
+                    <b-button
+                        id="submit-button"
+                        class="button"
+                        variant="success"
+                    >
+                        Submit
+                    </b-button>
+                </b-col>
+            </b-row>
         </b-container>
     </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 import Navbar from './Navbar';
 
 export default {
@@ -27,6 +85,23 @@ export default {
             default  : null,
         },
     },
+    data() {
+        return {
+            results : '',
+        };
+    },
+    computed : {
+        ...mapGetters(
+            [
+                'currentUser',
+            ],
+        ),
+    },
+    mounted() {
+        if ( ! this.currentUser ) {
+            this.$router.push( { name : 'login' } );
+        }
+    },
 };
 </script>
 
@@ -34,5 +109,21 @@ export default {
 <style scoped>
 #header {
     margin-top : 3%;
+}
+
+#results-col {
+    height : 500px;
+}
+
+.button {
+    margin : 5px;
+}
+
+.row {
+    padding : 5px;
+}
+
+div {
+    border : solid black 1px;
 }
 </style>
