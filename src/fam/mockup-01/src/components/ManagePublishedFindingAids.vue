f8e241ab1c6c09f70084048c1e97bedb4da9abe5
David <da70@nyu.edu>
2020-04-15 13:42:29 -0400

    FAM mockup 1: start work on table in ManagePublishedFindingAids

--- src/fam/mockup-01/src/components/ManagePublishedFindingAids.vue
+++ src/fam/mockup-01/src/components/ManagePublishedFindingAids.vue
@@ -9,11 +9,176 @@
             <h1 id="header">
                 Manage published finding aids
             </h1>
+
+            <b-row>
+                <b-col
+                    lg="6"
+                    class="my-1"
+                >
+                    <b-form-group
+                        label="Filter"
+                        label-cols-sm="3"
+                        label-align-sm="right"
+                        label-size="sm"
+                        label-for="filterInput"
+                        class="mb-0"
+                    >
+                        <b-input-group size="sm">
+                            <b-form-input
+                                id="filterInput"
+                                v-model="filter"
+                                type="search"
+                                placeholder="Type to Search"
+                            />
+                            <b-input-group-append>
+                                <b-button
+                                    :disabled="!filter"
+                                    @click="filter = ''"
+                                >
+                                    Clear
+                                </b-button>
+                            </b-input-group-append>
+                        </b-input-group>
+                    </b-form-group>
+                </b-col>
+
+                <b-col
+                    lg="6"
+                    class="my-1"
+                >
+                    <b-form-group
+                        label="Filter On"
+                        label-cols-sm="3"
+                        label-align-sm="right"
+                        label-size="sm"
+                        description="Leave all unchecked to filter on all data"
+                        class="mb-0"
+                    >
+                        <b-form-checkbox-group
+                            v-model="filterOn"
+                            class="mt-1"
+                        >
+                            <b-form-checkbox value="name">
+                                Name
+                            </b-form-checkbox>
+                            <b-form-checkbox value="age">
+                                Age
+                            </b-form-checkbox>
+                            <b-form-checkbox value="isActive">
+                                Active
+                            </b-form-checkbox>
+                        </b-form-checkbox-group>
+                    </b-form-group>
+                </b-col>
+
+                <b-col
+                    sm="5"
+                    md="6"
+                    class="my-1"
+                >
+                    <b-form-group
+                        label="Per page"
+                        label-cols-sm="6"
+                        label-cols-md="4"
+                        label-cols-lg="3"
+                        label-align-sm="right"
+                        label-size="sm"
+                        label-for="perPageSelect"
+                        class="mb-0"
+                    >
+                        <b-form-select
+                            id="perPageSelect"
+                            v-model="perPage"
+                            size="sm"
+                            :options="pageOptions"
+                        />
+                    </b-form-group>
+                </b-col>
+
+                <b-col
+                    sm="7"
+                    md="6"
+                    class="my-1"
+                >
+                    <b-pagination
+                        v-model="currentPage"
+                        :total-rows="totalRows"
+                        :per-page="perPage"
+                        align="fill"
+                        size="sm"
+                        class="my-0"
+                    />
+                </b-col>
+            </b-row>
+
+            <!-- Main table element -->
+            <b-table
+                show-empty
+                small
+                stacked="md"
+                :items="items"
+                :fields="fields"
+                :current-page="currentPage"
+                :per-page="perPage"
+                :filter="filter"
+                :filter-included-fields="filterOn"
+                :sort-by.sync="sortBy"
+                :sort-desc.sync="sortDesc"
+                :sort-direction="sortDirection"
+                @filtered="onFiltered"
+            >
+                <template v-slot:cell(name)="row">
+                    {{ row.value.first }} {{ row.value.last }}
+                </template>
+
+                <template v-slot:cell(actions)="row">
+                    <b-button
+                        size="sm"
+                        class="mr-1"
+                        @click="info(row.item, row.index, $event.target)"
+                    >
+                        Info modal
+                    </b-button>
+                    <b-button
+                        size="sm"
+                        @click="row.toggleDetails"
+                    >
+                        {{ row.detailsShowing ? 'Hide' : 'Show' }} Details
+                    </b-button>
+                </template>
+
+                <template v-slot:row-details="row">
+                    <b-card>
+                        <ul>
+                            <li
+                                v-for="(value, key) in row.item"
+                                :key="key"
+                            >
+                                {{ key }}: {{ value }}
+                            </li>
+                        </ul>
+                    </b-card>
+                </template>
+            </b-table>
+
+            <!-- Info modal -->
+            <b-modal
+                :id="infoModal.id"
+                :title="infoModal.title"
+                ok-only
+                @hide="resetInfoModal"
+            >
+                <pre>{{ infoModal.content }}</pre>
+            </b-modal>
         </b-container>
     </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
+import moment from 'moment';
+
 import Navbar from './Navbar';
 
 export default {
@@ -21,10 +186,96 @@ export default {
     components : {
         Navbar,
     },
-    props : {
-        msg : {
-            type     : String,
-            default  : null,
+    data() {
+        return {
+            fields : [
+                {
+                    key           : 'repository',
+                    label         : 'Repository',
+                    sortable      : true,
+                    sortDirection : 'desc',
+                },
+                {
+                    key      : 'title',
+                    label    : 'Title',
+                    sortable : true,
+                    class    : 'text-center',
+                },
+                {
+                    key               : 'datetime',
+                    label             : 'Datetime',
+                    formatter         : ( datetime ) => {
+                        moment( datetime ).format( 'MM/DD/YYYY h:mm a' );
+                    },
+                    sortable          : true,
+                    sortByFormatted   : true,
+                    filterByFormatted : true,
+                },
+                {
+                    key   : 'actions',
+                    label : 'Actions',
+                },
+            ],
+            totalRows     : 1,
+            currentPage   : 1,
+            perPage       : 5,
+            pageOptions   : [ 10, 25, 50, 100 ],
+            sortBy        : '',
+            sortDesc      : false,
+            sortDirection : 'asc',
+            filter        : null,
+            filterOn      : [],
+            infoModal     : {
+                id      : 'info-modal',
+                title   : '',
+                content : '',
+            },
+        };
+    },
+    computed   : {
+        items() {
+            const items = [];
+
+            Object.keys( this.publishedFindingAids ).forEach( repository => {
+                const findingAids = this.publishedFindingAids[ repository ];
+
+                Object.keys( findingAids ).forEach( id => {
+                    const findingAid = findingAids[ id ];
+
+                    items.push( [ repository, findingAid.title, findingAid.datetime ] );
+                } );
+            } );
+
+            return items;
+        },
+        ...mapGetters(
+            [
+                'currentRepositories',
+                'currentUser',
+                'publishedFindingAids',
+            ],
+        ),
+    },
+    watch      : {},
+    mounted() {
+        if ( ! this.currentUser ) {
+            this.$router.push( { name : 'login' } );
+        }
+    },
+    methods    : {
+        info( item, index, button ) {
+            this.infoModal.title = `Row index: ${ index }`;
+            this.infoModal.content = JSON.stringify( item, null, 2 );
+            this.$root.$emit( 'bv::show::modal', this.infoModal.id, button );
+        },
+        resetInfoModal() {
+            this.infoModal.title = '';
+            this.infoModal.content = '';
+        },
+        onFiltered( filteredItems ) {
+            // Trigger pagination to update the number of buttons/pages due to filtering
+            this.totalRows = filteredItems.length;
+            this.currentPage = 1;
         },
     },
 };
@@ -34,5 +285,26 @@ export default {
 <style scoped>
     #header {
         margin-top : 3%;
+    }
+
+    #results-col {
+        height : 500px;
+    }
+
+    #results-textarea {
+        border  : solid black 1px;
+        padding : 1%;
+    }
+
+    #upload-row {
+        padding : 2%;
+    }
+
+    .button {
+        margin : 1%;
+    }
+
+    .row {
+        padding : 1%;
     }
 </style>
