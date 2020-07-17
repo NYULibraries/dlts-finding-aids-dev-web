993e0eb8bdbfe09f683dda1c4fe40b5f75e2307c
David <da70@nyu.edu>
2020-07-17 12:57:28 -0400

    FAM mockup 1: add in-process fadesign_116_* fakes

--- src/fam/mockup-01/src/store/in-process-finding-aids.js
+++ src/fam/mockup-01/src/store/in-process-finding-aids.js
@@ -27,6 +27,14 @@ export default {
             'timestamp' : 1584539268,
             'title'     : 'New York University Archives Photograph Collection PHOTO.00001',
         },
+        'fadesign_116_changed' : {
+            'timestamp' : 936882549,
+            'title'     : 'The Changing In-Process Finding Aid',
+        },
+        'fadesign_116_deleted' : {
+            'timestamp' : 936882549,
+            'title'     : 'The Disappearing In-Process Finding Aid',
+        },
     },
     'bhs' : {
         '1974.002' : {
