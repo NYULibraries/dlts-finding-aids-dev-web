73595a805848239ce6849e153cabbcb082265855
David <da70@nyu.edu>
2021-01-12 16:22:43 -0500

    FAM mockup 1 -- CreateNewFindingAid: move requiredEADElementsGetters to top-level scope

--- src/fam/mockup-01/src/components/CreateNewFindingAid.vue
+++ src/fam/mockup-01/src/components/CreateNewFindingAid.vue
@@ -89,6 +89,89 @@ import { mapActions, mapGetters } from 'vuex';
 
 import Navbar from './Navbar';
 
+// This will fill this.uploadedFindingAid
+const requiredEADElementsGetters = {
+    eadid : function ( eadDoc ) {
+        const elementName = '<eadid>';
+        const element = eadDoc.getElementsByTagName( 'eadid' )[ 0 ];
+
+        let elementValue;
+        if ( element ) {
+            elementValue = element.textContent.trim();
+
+            if ( ! elementValue ) {
+                throw new Error( `ERROR: Required element ${ elementName } is empty.` );
+            }
+
+            return elementValue;
+        } else {
+            throw new Error( `ERROR: Required element ${ elementName } not found.` );
+        }
+    },
+    repository : function ( eadDoc ) {
+        const elementName = '<repository>/<corpname>';
+        let element;
+        let elementValue;
+        try {
+            element = eadDoc.getElementsByTagName( 'repository' )[ 0 ]
+                .getElementsByTagName( 'corpname' )[ 0 ];
+
+            elementValue = element.textContent.trim();
+        } catch( e ) {
+            throw new Error( `ERROR: Required element ${ elementName } not found.` );
+        }
+
+        if ( ! elementValue ) {
+            throw new Error( `ERROR: Required element ${ elementName } is empty.` );
+        }
+
+        return elementValue;
+    },
+    title : function ( eadDoc ) {
+        const errors = [];
+
+        const unitTitleElementName = '<archdesc>/<did>/<unittitle>';
+        let unitTitleElement;
+        let unitTitleElementValue;
+        try {
+            unitTitleElement = eadDoc.getElementsByTagName( 'archdesc' )[ 0 ]
+                .getElementsByTagName( 'did' )[ 0 ]
+                .getElementsByTagName( 'unittitle' )[ 0 ];
+
+            unitTitleElementValue = unitTitleElement.textContent.trim();
+
+            if ( ! unitTitleElementValue ) {
+                errors.push( `ERROR: Required element ${ unitTitleElementName } is empty.` );
+            }
+        } catch( e ) {
+            errors.push( `ERROR: Required element ${ unitTitleElementName } not found.` );
+        }
+
+        const unitIdElementName = '<archdesc>/<did>/<unitid>';
+        let unitIdElement;
+        let unitIdElementValue;
+        try {
+            unitIdElement = eadDoc.getElementsByTagName( 'archdesc' )[ 0 ]
+                .getElementsByTagName( 'did' )[ 0 ]
+                .getElementsByTagName( 'unitid' )[ 0 ];
+
+            unitIdElementValue = unitIdElement.textContent.trim();
+
+            if ( ! unitIdElementValue ) {
+                errors.push( `ERROR: Required element ${ unitIdElementName } is empty.` );
+            }
+        } catch( e ) {
+            errors.push( `ERROR: Required element ${ unitIdElementName } not found.` );
+        }
+
+        if ( errors.length > 0 ) {
+            throw new Error( errors.join( '\n\n' ) );
+        }
+
+        return unitTitleElementValue + ' ' + unitIdElementValue;
+    },
+};
+
 let parser;
 
 export default {
@@ -358,88 +441,6 @@ Some things to try:
                 return;
             }
 
-            // This will fill this.uploadedFindingAid
-            const requiredEADElementsGetters = {
-                eadid : function ( eadDoc ) {
-                    const elementName = '<eadid>';
-                    const element = eadDoc.getElementsByTagName( 'eadid' )[ 0 ];
-
-                    let elementValue;
-                    if ( element ) {
-                        elementValue = element.textContent.trim();
-
-                        if ( ! elementValue ) {
-                            throw new Error( `ERROR: Required element ${ elementName } is empty.` );
-                        }
-
-                        return elementValue;
-                    } else {
-                        throw new Error( `ERROR: Required element ${ elementName } not found.` );
-                    }
-                },
-                repository : function ( eadDoc ) {
-                    const elementName = '<repository>/<corpname>';
-                    let element;
-                    let elementValue;
-                    try {
-                        element = eadDoc.getElementsByTagName( 'repository' )[ 0 ]
-                            .getElementsByTagName( 'corpname' )[ 0 ];
-
-                        elementValue = element.textContent.trim();
-                    } catch( e ) {
-                        throw new Error( `ERROR: Required element ${ elementName } not found.` );
-                    }
-
-                    if ( ! elementValue ) {
-                        throw new Error( `ERROR: Required element ${ elementName } is empty.` );
-                    }
-
-                    return elementValue;
-                },
-                title : function ( eadDoc ) {
-                    const errors = [];
-
-                    const unitTitleElementName = '<archdesc>/<did>/<unittitle>';
-                    let unitTitleElement;
-                    let unitTitleElementValue;
-                    try {
-                        unitTitleElement = eadDoc.getElementsByTagName( 'archdesc' )[ 0 ]
-                            .getElementsByTagName( 'did' )[ 0 ]
-                            .getElementsByTagName( 'unittitle' )[ 0 ];
-
-                        unitTitleElementValue = unitTitleElement.textContent.trim();
-
-                        if ( ! unitTitleElementValue ) {
-                            errors.push( `ERROR: Required element ${ unitTitleElementName } is empty.` );
-                        }
-                    } catch( e ) {
-                        errors.push( `ERROR: Required element ${ unitTitleElementName } not found.` );
-                    }
-
-                    const unitIdElementName = '<archdesc>/<did>/<unitid>';
-                    let unitIdElement;
-                    let unitIdElementValue;
-                    try {
-                        unitIdElement = eadDoc.getElementsByTagName( 'archdesc' )[ 0 ]
-                            .getElementsByTagName( 'did' )[ 0 ]
-                            .getElementsByTagName( 'unitid' )[ 0 ];
-
-                        unitIdElementValue = unitIdElement.textContent.trim();
-
-                        if ( ! unitIdElementValue ) {
-                            errors.push( `ERROR: Required element ${ unitIdElementName } is empty.` );
-                        }
-                    } catch( e ) {
-                        errors.push( `ERROR: Required element ${ unitIdElementName } not found.` );
-                    }
-
-                    if ( errors.length > 0 ) {
-                        throw new Error( errors.join( '\n\n' ) );
-                    }
-
-                    return unitTitleElementValue + ' ' + unitIdElementValue;
-                },
-            };
             if ( ! this.getRequiredEADElements( eadDoc, requiredEADElementsGetters ) ) {
                 abort = true;
             }
