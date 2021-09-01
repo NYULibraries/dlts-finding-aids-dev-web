2d18d3489fb0483cc79a7e228ffe579811e1920d
David <da70@nyu.edu>
2021-09-01 19:07:04 -0400

    src/fam/mockup-0{1,2}/: update repository code -- change "bhs" to "cbh"

--- src/fam/mockup-01/src/store/store-config.js
+++ src/fam/mockup-01/src/store/store-config.js
@@ -42,7 +42,7 @@ const state = function () {
                 organization : 'NYU',
                 loadTime     : 0.101 * TOTAL_LOAD_TIME_FOR_ALL_REPOSITORIES,
             },
-            bhs      : {
+            cbh      : {
                 archivists   : [
                     'weatherly@nyu.edu',
                     'carol.kassel@nyu.edu',
@@ -50,7 +50,7 @@ const state = function () {
                     'maggie@brooklynhistory.org',
                 ],
                 name         : 'Center for Brooklyn History',
-                organization : 'BHS',
+                organization : 'CBH',
                 loadTime     : 0.062 * TOTAL_LOAD_TIME_FOR_ALL_REPOSITORIES,
             },
             fales    : {
